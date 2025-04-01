c8d476560ea4574ac89ea30bab656df168816e5e
Herculano De Biasi <02105018980@udesc.br>
2025-03-31 21:18:41 -0300

    Alterado nome e idade da classe OlaMundo

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,6 +1,6 @@
 public class OlaMundo {
-    String nome = "Udesc!";
-    int idade = 42;
+    String nome = "Curso de Sistemas de Informação!";
+    double idade = 1.5;
 
     void imprimirDados() {
         System.out.println(nome + " - " + idade);
