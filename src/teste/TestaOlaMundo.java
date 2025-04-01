8b11f6d47f598cef0007375de6e9ab7778843c13
Herculano De Biasi <02105018980@udesc.br>
2025-03-31 21:42:28 -0300

    Exemplo de leitura de dados com a classe Scanner

--- src/teste/TestaOlaMundo.java
+++ src/teste/TestaOlaMundo.java
@@ -1,10 +1,23 @@
 package teste;
 
+import java.util.Scanner;
+
 public class TestaOlaMundo {
     public static void main(String[] args) {
         //<nome classe> <nome do objeto> = new <nome classe>
         OlaMundo xyz = new OlaMundo();
         xyz.imprimirDados();
         xyz.imprimir("Herculano", 49);
+
+        // Criação de um objeto para leitura de dados
+        Scanner leDados = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = leDados.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int valor = leDados.nextInt();
+
+        xyz.imprimir(nome, valor);
     }
 }
