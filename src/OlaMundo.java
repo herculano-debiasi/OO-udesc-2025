cff87dd966c00af448b9e0099749046841056d27
Herculano De Biasi <02105018980@udesc.br>
2025-03-31 21:13:22 -0300

    Criação da classe OlaMundo e respectiva classe de teste TestaOlaMuno

---
+++ src/OlaMundo.java
@@ -0,0 +1,12 @@
+public class OlaMundo {
+    String nome = "Udesc!";
+    int idade = 42;
+
+    void imprimirDados() {
+        System.out.println(nome + " - " + idade);
+    }
+
+    void imprimir(String nome, int idade) {
+        System.out.println(nome + " - " + idade);
+    }
+}
---
+++ src/TestaOlaMundo.java
@@ -0,0 +1,8 @@
+public class TestaOlaMundo {
+    public static void main(String[] args) {
+        //<nome classe> <nome do objeto> = new <nome classe>
+        OlaMundo xyz = new OlaMundo();
+        xyz.imprimirDados();
+        xyz.imprimir("Herculano", 49);
+    }
+}
